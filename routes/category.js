c9fcc5a1331e641d4449c5d6c7aba3d550fe7349
biley02 <57981298+biley02@users.noreply.github.com>
2020-06-20 00:09:53 +0530

    Completed product and category routes

--- routes/category.js
+++ routes/category.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    create
+    create,
+    categoryById,
+    read,
+    list,
+    update,
+    remove,
 } = require("../controllers/category");
 const {
     isAuth,
@@ -13,7 +18,25 @@ const {
     userById
 } = require("../controllers/user");
 
+router.get("/category/:categoryId", read);
 router.post("/category/create/:userId", requireSignin, isAdmin, isAuth, create);
+router.put(
+    "/category/:categoryId/:userId",
+    requireSignin,
+    isAdmin,
+    isAuth,
+    update
+);
+router.delete(
+    "/category/:categoryId/:userId",
+    requireSignin,
+    isAdmin,
+    isAuth,
+    remove
+);
+router.get("/categories", list);
 
+//These middleware work when these is found in route
+router.param("categoryId", categoryById);
 router.param("userId", userById);
 module.exports = router;
--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    create
+    create,
+    productById,
+    read,
+    remove,
+    update,
+    list,
+    listRelated,
+    listCategories,
+    listBySearch,
+    photo,
 } = require("../controllers/product");
 const {
     isAuth,
@@ -13,7 +22,29 @@ const {
     userById
 } = require("../controllers/user");
 
+router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAdmin, isAuth, create);
+router.delete(
+    "/product/:productId/:userId",
+    requireSignin,
+    isAdmin,
+    isAuth,
+    remove
+);
+router.put(
+    "/product/:productId/:userId",
+    requireSignin,
+    isAdmin,
+    isAuth,
+    update
+);
+
+router.get("/products", list);
+router.get("/products/related/:productId", listRelated);
+router.get("/products/categories", listCategories);
+router.post("/products/by/search", listBySearch);
+router.get("/product/photo/:productId", photo);
 
 router.param("userId", userById);
+router.param("productId", productById);
 module.exports = router;
